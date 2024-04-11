d3d4d22870352684efdb42f974b011c0b4ca385f
Pratham Gupta <pratham.gupta2k@gmail.com>
2024-04-11 19:54:41 -0400

    Corrected CLI Tool setup

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    README = f.read()
+
 setup(
     name="LintAid",
     version="1.0.0",
-    packages=find_packages(),
+    description="CLI tool to help seamless integration of Linters",
+    long_description=README,
+    author="Pratham Gupta",
+    author_email="pratham.gupta@nyu.edu",
+    url="https://github.com/PrathamGupta/LintAid",
     entry_points={
         "console_scripts": [
-            "lintaid=lintaid:main",
+            "lintaid=LintAid.lintaid:main",
         ],
     },
     # Add other package details as needed
